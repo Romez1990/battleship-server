18e8fe6c3f9ffa86220093edc210fd1f1e287719
dizinnes <dizinnes228@gmail.com>
2021-03-16 12:59:32 +0600

    temp

--- src/game_session/game_session_handler.py
+++ src/game_session/game_session_handler.py
@@ -5,9 +5,10 @@ class GameSessionHandler(WebSocketHandler):
     def open(self) -> None:
         print('open')
         self.write_message('open message')
-
+        print('werqwer')
     def on_message(self, message):
         print(f'message: {message}')
-
+        print('werqwer')
     def on_close(self):
         print('close')
+        print('werqwer')
