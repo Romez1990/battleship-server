381e9d9450510395d5d8cc796687ff0f92b117a9
dizinnes <dizinnes228@gmail.com>
2021-03-17 03:48:42 +0600

    feat(http_handler): Add BaseHandler

--- src/rest_api/request_handlers/PlayerHandler.py
+++ src/rest_api/request_handlers/PlayerHandler.py
@@ -1,10 +1,16 @@
+import tornado.web
 import json
 
-import tornado.web
+from tornado import escape, httputil
+
+from typing import Optional
 
 
-class PlayerHandler(tornado.web.RequestHandler):
-    def post(self):
+from .BaseHandler import BaseHandler
+
+
+class PlayerHandler(BaseHandler, tornado.web.RequestHandler):
+    def post(self) -> None:
         data = json.loads(self.request.body)
         player = {
             'first_name': data.get('first_name'),
@@ -12,3 +18,11 @@ class PlayerHandler(tornado.web.RequestHandler):
             'year_of_birth': data.get('year_of_birth'),
             'grade': data.get('grade'),
         }
+        self.write({'player': player})  # TODO: Set the HTTP status code
+
+    def set_status(self, status_code: int, reason: Optional[str] = None) -> None:
+        self._status_code = status_code
+        if reason is not None:
+            self._reason = escape.native_str(reason)
+        else:
+            self._reason = httputil.responses.get(status_code, "Unknown")
