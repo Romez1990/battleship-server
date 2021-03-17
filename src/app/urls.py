f98080fd6417ad37c2fd3f6223fc3b98d1a8fb85
Romez <Romez1990@gmail.com>
2021-03-17 22:39:53 +0300

    refactor(app): move all routes to urls file

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,9 +1,12 @@
 from src.game_start import GameStartHandler
 from src.game_session import GameSessionHandler
+from src.rest_api.request_handlers import PlayerHandler, CreateGameHandler
 from .routes import HttpRoute, WebSocketRoute
 
 http_urls: list[HttpRoute] = [
     HttpRoute('start', GameStartHandler),
+    HttpRoute('player', PlayerHandler),
+    HttpRoute('create-game', CreateGameHandler),
 ]
 
 web_socket_urls: list[WebSocketRoute] = [
--- src/rest_api/__init__.py
+++
@@ -1,1 +0,0 @@
-from .urls import REST_API_URLS
