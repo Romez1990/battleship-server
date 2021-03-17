0bf45ea72915d2ce3a31c8ee06eeace470b1f50f
dizinnes <dizinnes228@gmail.com>
2021-03-17 21:09:57 +0600

    chore: Add TODO

--- src/rest_api/urls.py
+++ src/rest_api/urls.py
@@ -1,9 +1,10 @@
 from .request_handlers import (
     PlayerHandler,
-    CreateGame,
+    CreateGameHandler,
 )
 
+# TODO:  Delete `/api/v1/` and move it to GLOBAL urls.py which in the `app` directory
 REST_API_URLS = [
     (r'/api/v1/player/', PlayerHandler),
-    (r'/api/v1/create-game/', CreateGame),
+    (r'/api/v1/create-game/', CreateGameHandler),
 ]
