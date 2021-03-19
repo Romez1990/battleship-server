df260dc78752c7f354830c8bf9822f8fbbd39fb8
DiZiNnEs <dizinnes228@gmail.com>
2021-03-19 18:04:14 +0600

    bugfix(rest_api): Add service decorator

--- src/rest_api/create_game/create_game_service_impl.py
+++ src/rest_api/create_game/create_game_service_impl.py
@@ -4,6 +4,9 @@ from src.ioc_container import service
 from .create_game_service import CreateGameService
 
 
+
+
+@service
 class CreateGameServiceImpl(CreateGameService):
     def get_game_code(self) -> Union[str, bytes, dict]:
         return {'code': 222}  # TODO: Create logic
