d231010e738ed18d1b8d34a104a511c3ac54585f
Romez <Romez1990@gmail.com>
2021-06-20 20:31:28 +0300

    temp

--- show_questions.py
+++ show_questions.py
@@ -2,8 +2,8 @@ from src.questions.question_service_impl import QuestionServiceImpl
 
 questions = QuestionServiceImpl()
 for question in questions._QuestionServiceImpl__questions:
+    question = question.to_model()
     print(question.text)
     for answer in question.answers:
         print(f'- {answer}')
-    print()
     print()
